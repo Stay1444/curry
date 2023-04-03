85d7a3d24cfbc7c4b91db0c8ce94f9a4b9a7bf1a
Stay1444 <quiquelluc@gmail.com>
2023-04-03 21:57:21 +0200

    Native self-extract

--- CurryEngine.Editor/Program.cs
+++ CurryEngine.Editor/Program.cs
@@ -1,6 +1,20 @@
-﻿using CurryEngine.Editor;
+﻿using System.Reflection;
+using CurryEngine.Editor;
 using Microsoft.Xna.Framework;
 using Serilog;
+
+{
+    foreach (var resource in Assembly.GetAssembly(typeof(Program))!.GetManifestResourceNames()
+                 .Where(x => x.StartsWith("CurryEngine.Editor.Resources.Lib")))
+    {
+        var fileName = resource.Replace("CurryEngine.Editor.Resources.Lib.", "");
+        if (File.Exists(fileName)) continue;
+        await using var fsr = Assembly.GetAssembly(typeof(Program))!.GetManifestResourceStream(resource);
+        await using var fsw = File.OpenWrite(fileName);
+        await fsr!.CopyToAsync(fsw);
+        Console.WriteLine($"Extracted {resource}");
+    }
+}
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
